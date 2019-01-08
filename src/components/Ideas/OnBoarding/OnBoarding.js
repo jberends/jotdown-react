38dac7ebee114de915c01e40e5729106b25fe23b
Jochem Berends <jochem.berends@ke-works.com>
2019-01-08 21:58:52 +0100

    using improved ideas from newideas into onboarding. especially the select all

--- src/components/Ideas/OnBoarding/OnBoarding.js
+++ src/components/Ideas/OnBoarding/OnBoarding.js
@@ -7,9 +7,9 @@ import ContentEditable from "react-contenteditable";
 class OnBoarding extends React.Component {
   constructor(props) {
     super(props);
-    this.welcomeText = "Your name here";
+    this.defaults = {welcomeText: "Your name..."};
     this.state = {
-      username: props.account ? props.account.username : this.welcomeText,
+      username: props.account ? props.account.username : this.defaults.welcomeText,
       showSnack: false
     };
   }
@@ -25,9 +25,19 @@ class OnBoarding extends React.Component {
   };
 
   handleAccountName = evt => {
-    if (this.state.username !== this.welcomeText) {
+    if (this.state.username !== this.defaults.welcomeText) {
       this.props.registerHandler(this.state.username);
     }
+  };
+
+  handleFocus = evt => {
+    // from: https://stackoverflow.com/questions/6139107/programmatically-select-text-in-a-contenteditable-html-element/6150060#6150060
+    // document.execCommand('selectAll',false,null)
+    const range = document.createRange();
+    range.selectNodeContents(evt.target);
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
   };
 
   render() {
@@ -42,6 +52,8 @@ class OnBoarding extends React.Component {
             onChange={this.handleChange}
             onBlur={this.handleAccountName}
             onKeyPress={this.handleKeyPress}
+            onFocus={this.handleFocus}
+            tabIndex="1"
           />
           <span className="punctuation">{this.state.username !== this.welcomeText ? "." : "..."}</span>
         </span>
